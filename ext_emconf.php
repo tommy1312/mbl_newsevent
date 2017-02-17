5b6568a7df19ed18053df7cb2de2429adef8a8a3
mabolek <mathias@lilio.com>
2017-02-17 14:32:51 +0100

    Updated version number

--- ext_emconf.php
+++ ext_emconf.php
@@ -21,7 +21,7 @@ $EM_CONF[$_EXTKEY] = array (
   'author' => 'Mathias Bolt Lesniak',
   'author_email' => 'mathias@pixelant.no',
   'author_company' => 'Pixelant',
-  'version' => '2.6.0',
+  'version' => '7.6.0',
   '_md5_values_when_last_written' => 'a:20:{s:20:"class.ext_update.php";s:4:"a347";s:23:"class.mbl_newsevent.php";s:4:"87de";s:19:"event_template.tmpl";s:4:"3435";s:12:"ext_icon.gif";s:4:"c028";s:17:"ext_localconf.php";s:4:"a4ee";s:14:"ext_tables.php";s:4:"7b8b";s:14:"ext_tables.sql";s:4:"9c7e";s:28:"ext_typoscript_constants.txt";s:4:"b736";s:24:"ext_typoscript_setup.txt";s:4:"763a";s:13:"locallang.xml";s:4:"b4fc";s:16:"locallang_db.xml";s:4:"1062";s:34:"tt_news_v2_template_newsevent.html";s:4:"25f6";s:14:"doc/manual.sxw";s:4:"096b";s:19:"doc/wizard_form.dat";s:4:"1b9f";s:20:"doc/wizard_form.html";s:4:"2a29";s:21:"res/ics_template.tmpl";s:4:"e156";s:24:"static/ics/constants.txt";s:4:"68b3";s:20:"static/ics/setup.txt";s:4:"720e";s:29:"static/standard/constants.txt";s:4:"e2a5";s:25:"static/standard/setup.txt";s:4:"7804";}',
   'constraints' =>
   array (
